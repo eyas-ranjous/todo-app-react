8b805413ebbfd64a8057e8373b474ffb47279fc6
Eyas Ranjous <eyas.ranjous@gmail.com>
2020-06-24 00:13:28 -0500

    test state with button click

--- test/App/TodoFilters/index.test.js
+++ test/App/TodoFilters/index.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { mount } from 'enzyme';
 import { Provider } from 'react-redux'
 import createStore from '../../../app/store';
+import { TodoFiltersEnum } from '../../../app/store/TodoFiltersEnum';
 import TodoFilters from '../../../app/App/TodoFilters';
 
 describe('TodoFilters', () => {
@@ -15,5 +16,18 @@ describe('TodoFilters', () => {
       </Provider>
     );
     expect(wrapper).toMatchSnapshot();
+  });
+
+  it('set filter state when a filter button is clicked', () => {
+    const setFilter = jest.fn();
+    const wrapper = mount(
+      <Provider store={store}>
+        <TodoFilters />
+      </Provider>
+    );
+    wrapper.find('.filter-button-done').simulate('click');
+    const { todoFilterSet, selectedFilter } = store.getState();
+    expect(Array.from(todoFilterSet)).toEqual([TodoFiltersEnum.DONE]);
+    expect(selectedFilter).toEqual(TodoFiltersEnum.DONE);
   });
 });
